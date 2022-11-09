a6f4cd7d4f7f3a818c0884e53d6e0af5d974f3fa
Ryland Adams <rylandadams@yahoo.com>
2022-11-09 10:19:54 -0800

    accounting for capitalization

--- quiz_game.py
+++ quiz_game.py
@@ -7,6 +7,26 @@ if playing != "yes":
 
 print("okay lets play! ")
 
-answer = input("what does CPU syand for? ")
-if answer == "central processing unit":
+answer = input("what does CPU stand for? ")
+if answer.lower() == "central processing unit":
     print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("what does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("what does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("what does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
