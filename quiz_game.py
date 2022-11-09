8aff59d19818e40df6273337c84e30f26e30f1c0
Ryland Adams <rylandadams@yahoo.com>
2022-11-09 10:09:14 -0800

    first question

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,12 @@
 print("Welcome to the quiz")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("okay lets play! ")
+
+answer = input("what does CPU syand for? ")
+if answer == "central processing unit":
+    print("Correct!")
